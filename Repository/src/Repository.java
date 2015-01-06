e7dd17c0c938233cb6a7aac70d846814b7eaf5cb
Krupel <gncserrano@gmail.com>
2015-01-06 01:17:37 +0000

    verify file on start

--- Repository/src/Repository.java
+++ Repository/src/Repository.java
@@ -1,9 +1,11 @@
+import shared.OmniFile;
 import shared.OmniRepository;
 import threads.AnswerClient;
 import threads.DirectoryHandler;
 import threads.HeartBeat;
 import threads.ProcessServer;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -27,12 +29,23 @@ public class Repository {
             }
         }
 
+
         switch (args.length) {
             case 3:
                 port = Integer.parseInt(args[0]);
                 address = args[1].trim();
                 filesDirectory = args[2].trim();
+                File folder = new File(filesDirectory);
+
                 repo = new OmniRepository(port,address,filesDirectory, "");
+
+                //Add Files in directory on start
+                for (final File fileEntry : folder.listFiles()) {
+                    if (!fileEntry.isDirectory()) {
+                        repo.getFileList().add(new OmniFile(filesDirectory+fileEntry.getName()));
+                    }
+                }
+
                 answerClient = new AnswerClient(repo);
                 answerClient.start();
                 new HeartBeat(repo).start();
