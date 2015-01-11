97e24297d734640d9febbde43b3fc8982cf31702
Mário Silva <mjgs.biz@gmail.com>
2015-01-11 22:47:39 +0000

    Constants refactor

--- OmniBoxShared/src/shared/Constants.java
+++ OmniBoxShared/src/shared/Constants.java
@@ -9,21 +9,22 @@ import java.io.Serializable;
  * Created by Omnibox on 02/11/14.
  */
 public class Constants implements Serializable {
-    final public static String MULTICAST_ADDRESS = "230.30.30.30";
-    final public static int MAX_SIZE = 1024*2;
-    final public static int TIMEOUT = 5 * 1000;
-    final public static int EXPIRE_TIME = 30 * 1000;
-    final public static String REQUEST_SERVER_IP_ADDRESS = "request_server_ip_address";
+    public static final String MULTICAST_ADDRESS = "230.30.30.30";
+    public static final int MULTICAST_PORT = 7000;
+    public static final int MAX_SIZE = 1024*2;
+    public static final int TIMEOUT = 5 * 1000;
+    public static final int EXPIRE_TIME = 30 * 1000;
+    public static final String REQUEST_SERVER_IP_ADDRESS = "request_server_ip_address";
 
-    final public static boolean FILEOK = true;
-    final public static boolean FILENOTOK = false;
-    final public static int OP_DOWNLOAD = 0;
-    final public static int OP_UPLOAD = 1;
-    final public static int OP_DELETE = 2;
-    final public static int OP_REPLICATION = 3;
-    final public static int OP_S_STARTED = 1;
-    final public static int OP_S_FINISHED = 0;
-    final public static int INACTIVE = 9;
+    public static final boolean FILEOK = true;
+    public static final boolean FILENOTOK = false;
+    public static final int OP_DOWNLOAD = 0;
+    public static final int OP_UPLOAD = 1;
+    public static final int OP_DELETE = 2;
+    public static final int OP_REPLICATION = 3;
+    public static final int OP_S_STARTED = 1;
+    public static final int OP_S_FINISHED = 0;
+    public static final int INACTIVE = 9;
 
     /**
      * Commands enumeration list.
